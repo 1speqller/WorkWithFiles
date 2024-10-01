626556806f91f726a6e5555c145ebd76aa6e389c
1speqller <hoshigake2000@gmail.com>
2024-10-01 13:02:44 +0300

    Создание метода, который удаляет каталог по указанному пути, если он использовался менее 30 минут назад

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿string path = "/Users/hoshi/Desktop/NewCopy";
+
+DeleteDirectory(path);
+static void DeleteDirectory(string path)
+{
+    DirectoryInfo dir = new DirectoryInfo(path);
+
+    if (dir.Exists && (dir.LastAccessTime < DateTime.Now - TimeSpan.FromMinutes(30)))
+    {
+        try
+        {
+            Console.WriteLine("Указанный каталог существует, приступаем к удалению...");
+            Directory.Delete(path, true);
+            Console.WriteLine("Каталог успешно удалены");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    else if (!dir.Exists)
+        Console.WriteLine("Каталог по указанному пути не существует!");
+
+    else if (dir.LastAccessTime > DateTime.Now - TimeSpan.FromMinutes(30))
+        Console.WriteLine("Каталог использовался менее 30 минут назад!");
+}
