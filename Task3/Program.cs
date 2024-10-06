511ef78acb9075f365f74ea14e028e5add6ec887
1speqller <hoshigake2000@gmail.com>
2024-10-06 13:58:07 +0300

    создание методов для анализа и удаления указанной директории

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,31 +1,37 @@
-﻿string path = "/Users/hoshi/Desktop/Проекты";
-DirectoryInfo dir = new DirectoryInfo(path);
-var folders = dir.GetDirectories();
+﻿string path = "/ProgramData/ВеселаяФерма2/sys";
+bool IsDelete = false;
 
-WriteDirectoryInfo(folders);
 
-Console.WriteLine($"Всего директория занимает: {DirSize(dir)}");
+Console.WriteLine($"Исходный размер папки: {UrlDirSize(path)}");
+long deleteEqual = UrlDirSize(path);
+DeleteDirectory(path, ref IsDelete);
+if (IsDelete)
+    Console.WriteLine($"Освобождено: {deleteEqual}: ");
+else
+    Console.WriteLine("Освобождено: 0");
+Console.WriteLine($"Текущий размер папки: {UrlDirSize(path)}");
 
-static void WriteDirectoryInfo(DirectoryInfo[] folders)
+static long UrlDirSize(string path)
 {
-    Console.WriteLine();
-    Console.WriteLine("Папки: ");
-    Console.WriteLine();
-
-    foreach (var folder in folders)
+    try
     {
-        try
+        DirectoryInfo dir = new DirectoryInfo(path);
+
+        if (dir.Exists)
+            return DirSize(dir);
+
+        else
         {
-            Console.WriteLine(folder.Name + $"- {DirSize(folder)} байт");
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(folder.Name + $"Не удалось рассчитать размер: {e.Message}");
+            Console.WriteLine("Каталог по указанному пути не существует!");
+            return 0;
         }
     }
-
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+        return 0;
+    }
 }
-
 static long DirSize(DirectoryInfo d)
 {
     long size = 0;
@@ -38,4 +44,31 @@ static long DirSize(DirectoryInfo d)
         size += DirSize(dir);
 
     return size;
+}
+static void DeleteDirectory(string path, ref bool IsDelete)
+{
+    
+    DirectoryInfo dir = new DirectoryInfo(path);
+
+    if (dir.Exists && (dir.LastAccessTime < DateTime.Now - TimeSpan.FromMinutes(30)))
+    {
+        try
+        {
+            Console.WriteLine("Указанный каталог существует, приступаем к удалению...");
+            Directory.Delete(path, true);
+            Console.WriteLine("Каталог успешно удалены");
+            IsDelete = true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    else if (!dir.Exists)
+        Console.WriteLine("Каталог по указанному пути не существует!");
+    
+        
+    else if (dir.LastAccessTime > DateTime.Now - TimeSpan.FromMinutes(30))
+        Console.WriteLine("Каталог использовался менее 30 минут назад!");
 }
