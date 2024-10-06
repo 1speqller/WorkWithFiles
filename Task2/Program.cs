64e9b43c228423c9d88c456d2506304af130efb4
1speqller <hoshigake2000@gmail.com>
2024-10-06 13:30:54 +0300

    Создание метода на анализ указанной директории и шаблона для выполнения task3

---
+++ Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿string path = "/Users/hoshi/Desktop/Проекты";
+
+
+Console.WriteLine($"Всего директория занимает: {UrlDirSize(path)}");
+
+static long UrlDirSize(string path)
+{
+    try
+    {
+        DirectoryInfo dir = new DirectoryInfo(path);
+        
+        if (dir.Exists)
+            return DirSize(dir);
+
+        else
+        {
+            Console.WriteLine("Каталог по указанному пути не существует!");
+            return 0;
+        }
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+        return 0;
+    }
+}
+
+static long DirSize(DirectoryInfo d)
+{
+    long size = 0;
+    FileInfo[] fis = d.GetFiles();
+    foreach (FileInfo fi in fis)
+        size += fi.Length;
+
+    DirectoryInfo[] dirs = d.GetDirectories();
+    foreach (DirectoryInfo dir in dirs)
+        size += DirSize(dir);
+
+    return size;
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,41 @@
+﻿string path = "/Users/hoshi/Desktop/Проекты";
+DirectoryInfo dir = new DirectoryInfo(path);
+var folders = dir.GetDirectories();
+
+WriteDirectoryInfo(folders);
+
+Console.WriteLine($"Всего директория занимает: {DirSize(dir)}");
+
+static void WriteDirectoryInfo(DirectoryInfo[] folders)
+{
+    Console.WriteLine();
+    Console.WriteLine("Папки: ");
+    Console.WriteLine();
+
+    foreach (var folder in folders)
+    {
+        try
+        {
+            Console.WriteLine(folder.Name + $"- {DirSize(folder)} байт");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(folder.Name + $"Не удалось рассчитать размер: {e.Message}");
+        }
+    }
+
+}
+
+static long DirSize(DirectoryInfo d)
+{
+    long size = 0;
+    FileInfo[] fis = d.GetFiles();
+    foreach (FileInfo fi in fis)
+        size += fi.Length;
+
+    DirectoryInfo[] dirs = d.GetDirectories();
+    foreach (DirectoryInfo dir in dirs)
+        size += DirSize(dir);
+
+    return size;
+}
